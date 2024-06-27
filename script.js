04a637315b8a1d6a58fdd254345f58b3bf20de40
OsmanBuyukcerci <osmanbuyukcerci17@gmail.com>
2024-06-27 11:35:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let input_email;
+let wrapper_success = document.getElementById("wrapper-success");
+
+document.addEventListener("DOMContentLoaded", () => {
+  let screen_width = screen.width;
+
+  let mobile_img = document.getElementById("mobile-img");
+  let desktop_img = document.getElementById("desktop-img");
+
+  if (screen_width && mobile_img && desktop_img && screen_width < 768) {
+    desktop_img.style.display = "none";
+  } else {
+    mobile_img.style.display = "none";
+  }
+});
+
+window.addEventListener("resize", () => {
+  let screen_width = screen.width;
+
+  let mobile_img = document.getElementById("mobile-img");
+  let desktop_img = document.getElementById("desktop-img");
+
+  if (screen_width && mobile_img && desktop_img && screen_width < 768) {
+    mobile_img.style.display = "block";
+    desktop_img.style.display = "none";
+  } else {
+    desktop_img.style.display = "block";
+    mobile_img.style.display = "none";
+  }
+});
+
+document.getElementById("email").addEventListener("input", (e) => {
+  input_email = e.target.value;
+});
+
+document.getElementById("form-email").addEventListener("submit", (event) => {
+  event.preventDefault();
+  wrapper_success.style.visibility = "visible";
+  if (wrapper_success) {
+    let span_email = document.getElementById("span-email");
+    span_email.innerText = input_email;
+  }
+});
+
+document.getElementById("btn-dismiss").addEventListener("click", () => {
+  if (wrapper_success) {
+    wrapper_success.style.visibility = "hidden";
+  }
+});
